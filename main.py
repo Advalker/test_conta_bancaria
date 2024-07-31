49153dde0fc133cf2d16a0f96840d5919c8981b7
AMF-TI <advalkers@gmail.com>
2024-07-31 14:48:36 -0300

    segundo commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 class ContaBanco:
 
-    def __init__(self, nome, saldo):
+    def __init__(self, nome, saldo = 0):
         self.nome = nome
         self.saldo = saldo
 
@@ -9,3 +9,19 @@ class ContaBanco:
 
     def saldo_em_conta(self):
         return self.saldo    
+    
+    def sacar(self, saldo):
+        if saldo <= self.saldo:
+            self.saldo = self.saldo - saldo
+            return self.saldo
+        else:
+            print(f"Saldo insuficiente! Seu saldo é de {self.saldo}.")
+
+
+conta = ContaBanco("valker") 
+conta.depositar(200)
+print(conta.saldo_em_conta())    
+conta.sacar(50)
+print(conta.saldo_em_conta())     
+conta.sacar(250)
+  
